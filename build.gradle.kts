29ace057dd84ebe980334bf9ff9cd4c9ae170c5a
Jose Amatitla <jose.amatitla@kroger.com>
2024-02-26 13:04:34 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.9"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.jose.kroger.sample"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudAzureVersion"] = "5.9.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.azure.spring:spring-cloud-azure-starter-actuator")
+	implementation("com.azure.spring:spring-cloud-azure-starter-data-cosmos")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.azure.spring:spring-cloud-azure-dependencies:${property("springCloudAzureVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
